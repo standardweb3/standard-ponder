9709974d7e29132a79ad6b4709dc7f66bef89096
Hyungsuk Kang <hskang9@gmail.com>
2024-07-06 16:17:24 +0900

    remove redundant code

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,6 @@ const io = require('socket.io')(server, {
   transports: ['trade', 'order', 'day', 'hour', 'min']
 });
 
-let tick = 0;
-// 1. listen for socket connections
 io.on('connection', (client: { emit: (arg0: string, arg1: { name: number; value: any; }) => void; }) => {
   
 });
