2657eec30073d92bce3f6cc4b51f92849acd1d63
Hyungsuk Kang <hskang9@gmail.com>
2024-07-06 16:19:21 +0900

    add connected message

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,9 @@ const io = require('socket.io')(server, {
   transports: ['trade', 'order', 'day', 'hour', 'min']
 });
 
-io.on('connection', (client: { emit: (arg0: string, arg1: { name: number; value: any; }) => void; }) => {
-  
+io.on('connection', (client: { emit: (arg0: string, arg1: { message: string }) => void; }) => {
+  // Send a 'connected' message to the client
+  client.emit('connected', { message: 'Welcome! You are connected to the server.' });
 });
 
 server.listen(process.env.WS_PORT);
